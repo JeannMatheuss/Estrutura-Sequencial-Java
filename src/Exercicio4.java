9f2472c872ebc430aff57720a9ba8e9a8ced579d
Jean <jeanmatheus0612@gmail.com>
2024-12-07 23:49:39 -0300

    Ultimo exercicio do Scanner

---
+++ src/Exercicio4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        
+        double grauDigitado;
+        Scanner grau = new Scanner(System.in);
+
+        System.out.println("Digite o valor em graus: ");
+        grauDigitado = grau.nextDouble();
+
+        double seno;
+        double cosseno;
+        double tangente;
+
+        //Tenho que terminar isso daqui 
+    }
+}
